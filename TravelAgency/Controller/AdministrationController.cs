3372ecd176ca0dde360988cc2225bac301d421fd
Krzysztof Wasik <Krzysztof.Wasik2002@gmail.com>
2024-06-05 19:18:19 +0200

    Updated user management and role assignment functionality updated administrator panel added user management roles to administrator panel

--- TravelAgency/Controller/AdministrationController.cs
+++ TravelAgency/Controller/AdministrationController.cs
@@ -1,9 +1,77 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TravelAgency.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 [Authorize(Roles = "Administrator")]
 public class AdministrationController : Controller
 {
-    public IActionResult Index() =>
-        View();
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public AdministrationController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var users = await _userManager.Users.ToListAsync();
+        return View(users);
+    }
+
+    public async Task<IActionResult> ManageUserRoles(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+        var model = new ManageUserRolesViewModel
+        {
+            UserId = userId,
+            UserRoles = userRoles,
+            Roles = _roleManager.Roles.Select(r => r.Name)
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ManageUserRoles(ManageUserRolesViewModel model)
+    {
+        var user = await _userManager.FindByIdAsync(model.UserId);
+
+        if (user == null)
+        {
+            ViewBag.ErrorMessage = $"User with Id = {model.UserId} cannot be found";
+            return View("NotFound");
+        }
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+        var result = await _userManager.RemoveFromRolesAsync(user, userRoles);
+
+        if (!result.Succeeded)
+        {
+            ModelState.AddModelError("", "Cannot remove user existing roles");
+            return View(model);
+        }
+
+        model.SelectedRoles = model.SelectedRoles ?? new List<string>();
+        result = await _userManager.AddToRolesAsync(user, model.SelectedRoles);
+
+        if (!result.Succeeded)
+        {
+            ModelState.AddModelError("", "Cannot add selected roles to user");
+            return View(model);
+        }
+
+        return RedirectToAction("Index");
+    }
+
 }
+
---
+++ TravelAgency/ViewModels/ManageUserRolesViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelAgency.ViewModels
+{
+    public class ManageUserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public IEnumerable<string> UserRoles { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+        public IEnumerable<string> SelectedRoles { get; set; }
+    }
+
+}
